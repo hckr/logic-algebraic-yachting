ff0aabedd1509b8b3d677bd148d6afcc93d7e408
Luke <luke@luke.co.pl>
2017-01-14 17:31:15 +0100

    Make parsing a little more flexible

--- parsing.js
+++ parsing.js
@@ -1,10 +1,27 @@
 function parseData(text) {
-    let groups = text.split('\n\n'),
-        results = {};
-    for (let group of groups) {
-        let lines = group.split('\n').map(l => l.trim()),
-            sectionName = lines.shift().replace(/:$/, '');
-        results[sectionName] = parseSection(sectionName, lines);
+    let lines = text.split('\n').map(l => l.trim()),
+        results = {},
+        currentSectionName,
+        currentSectionStartIndex;
+    for (let i = 0, len = lines.length; i < len; ++i) {
+        let line = lines[i];
+        if (line.length == 0) {
+            continue;
+        }
+        if (line.match(/:$/)) {
+            let newSectionName = line.replace(/:$/, '');
+            if (Object.keys(results).indexOf(newSectionName) != -1 ||
+                    currentSectionName == newSectionName) {
+                throw new Error(`Found duplicated section "${newSectionName}" at line ${i + 1}`)
+            }
+            if (currentSectionName) {
+                results[currentSectionName] =
+                    parseSection(currentSectionName,
+                        lines.slice(currentSectionStartIndex + 1, i).filter(l => l.length != 0));
+            }
+            currentSectionName = newSectionName;
+            currentSectionStartIndex = i;
+        }
     }
     return results;
 }
@@ -33,7 +50,7 @@ function parseSection(sectionName, lines) {
 
 function parseVariables(lines) {
     let variables = {};
-    lines.map(line => {
+    lines.forEach(line => {
         let [id, name] = line.split(':').map(l => l.trim());
         if (Object.keys(variables).indexOf(id) != -1) {
             throw id;
